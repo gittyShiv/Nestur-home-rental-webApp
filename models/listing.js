0bacc1d400bdf669b002ffb627ab52a61eae9a21
Shivam Maurya <shivamvision07@gmail.com>
2025-10-22 00:04:25 +0530

    updated

--- models/listing.js
+++ models/listing.js
@@ -1,52 +1,39 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const Review = require("./review.js");
+const Review = require("./review.js")
 
-const listingSchema = new Schema ({
-    title : {
-        type : String,
-        required:true,
+const listingSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
     },
-    description : String,
-    image : {
-        url:String,
-        filename:String,
-    }, 
-    price : Number,
-    location : String,
-    country : String,
-
-    reviews : [
+    description: String,
+    image: {
+        url: String,
+        filename: String,
+    },
+    
+    price: Number,
+    category: String,
+    location: String,
+    country: String,
+    reviews: [
         {
-            type : Schema.Types.ObjectId,
-            ref : "Review",
-        }
-    ],
-    owner : {
-        type : Schema.Types.ObjectId,
-        ref : "User",
-    },
-    geometry : {
-        type: {
-          type: String, // Don't do `{ location: { type: String } }`
-          enum: ['Point'], // 'location.type' must be 'Point'
-          required: true
+            type: Schema.Types.ObjectId,
+            ref: "Review",
         },
-        coordinates: {
-          type: [Number],
-          required: true
-        }
+    ],
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
     },
-  
 });
 
 listingSchema.post("findOneAndDelete", async (listing) => {
-    if(listing){
-        await Review.deleteMany({_id : {$in : listing.reviews}});
+    if(listing) {
+        await Review.deleteMany({_id : {$in: listing.reviews}});
     }
-    
-});
+})
 
-const Listing = mongoose.model("Listing",listingSchema);
-
+const Listing = mongoose.model("Listing", listingSchema);
 module.exports = Listing;
