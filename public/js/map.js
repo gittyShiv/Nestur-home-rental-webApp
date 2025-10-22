959a119bb871d9548c051f7c68c1b5078c7b131d
Shivam Maurya <shivamvision07@gmail.com>
2025-10-22 11:00:03 +0530

    updated

--- public/js/map.js
+++ public/js/map.js
@@ -1,13 +1,19 @@
-mapboxgl.accessToken =maptoken;
-const map = new mapboxgl.Map ({
-container: "map", // container ID
-// Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-style: "mapbox://styles/mapbox/streets-v12", // style URL
-center: coordinates, // starting position ling, lat]
-zoom: 9, // starting zoom
-})
-const marker = new mapboxgl.Marker({color:"red"})
- .setLngLat ( [12.554729, 55.70651]) //Listing-geometry-coordinates
- .addTo (map) ;
- 
+
+
+	mapboxgl.accessToken = mapToken;
+    const map = new mapboxgl.Map({
+   container: 'map', // container ID
+     center: listing.geometry.coordinates, // starting position [lng, lat]
+     style: 'mapbox://styles/mapbox/streets-v9', 
+     zoom: 9 // starting zoom
+    });
+
+   
+    // Create a new marker.
+    const marker = new mapboxgl.Marker({color : "red"})
+    .setLngLat(listing.geometry.coordinates) //listing/geomertyu / coordinatre
+    .setPopup(new mapboxgl.Popup({offset: 25})
+    
+    .setHTML(`<h4>${listing.title}</h4><p>Exact location provided after booking!</p>`))
+    .addTo(map);
  
